2137318ecb8ba73736c36e90a31bfba42d063ae2
Murugapriya <muruga5lakshmiaji1997@gmail.com>
2017-06-07 16:12:30 +0530

    Create priya.c

---
+++ priya.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%s",&a);
+if((a=='a')||(e=='e')||(i=='i')||(o=='o')||(u=='u'))
+{
+printf("%s is a vowel");
+}
+else
+{
+printf("%s is a constant")
+}
+return 0;
+}
